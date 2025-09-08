e46b11f1a8508cfdbc8534c91bfc518f31558864
UncannyLad <etietopetim@gmail.com>
2025-09-08 12:25:17 +0100

    removed some mistakes

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const textBox = document.getElementById('textBox');
 const category = document.getElementById('category');
 const taskList = document.getElementById('task-list');
 
-// const form = document.getElementById('task-form');
-// const taskInput = document.getElementById('task-input');
-// const categorySelect = document.getElementById('category-select');
-// const taskList = document.getElementById('task-list');
+
 
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
